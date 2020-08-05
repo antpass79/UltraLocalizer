2348aec0e1834b79942417b9ad48e45207a80073
Matteo Piazze <m.piazze-alten@esaote.com>
2020-08-05 11:27:42 +0200

    Feature Text box preview string different styles

--- Client/Globe.Client.Platform/Models/PreviewStyleInfo.cs
+++ Client/Globe.Client.Platform/Models/PreviewStyleInfo.cs
@@ -2,7 +2,7 @@
 using System.Windows.Controls;
 using System.Windows.Media;
 
-namespace Globe.Client.Platform.Controls
+namespace Globe.Client.Platform.Models
 {
     public class PreviewStyleInfo
     {
---
+++ Client/Globe.Client.Platform/Services/IPreviewStyleService.cs
@@ -0,0 +1,10 @@
+﻿using Globe.Client.Platform.Models;
+
+namespace Globe.Client.Platform.Services
+{
+    public interface IPreviewStyleService
+    {
+        PreviewStyleInfo this[string contextName] { get; }
+        PreviewStyleInfo this[string typeName, string contextName] { get; }
+    }
+}
