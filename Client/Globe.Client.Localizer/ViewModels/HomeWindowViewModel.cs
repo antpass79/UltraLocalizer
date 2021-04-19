376b419f5df813402404026e3beb638100658c8c
Matteo Piazze <m.piazze-alten@esaote.com>
2021-04-19 14:53:32 +0200

    Get Version service Removed

--- Client/Globe.Client.Localizer/ViewModels/HomeWindowViewModel.cs
+++ Client/Globe.Client.Localizer/ViewModels/HomeWindowViewModel.cs
@@ -11,26 +11,16 @@ namespace Globe.Client.Localizer.ViewModels
 {
     internal class HomeWindowViewModel : LocalizeWindowViewModel
     {
-        //private readonly ICompareVersionService _compareVersionService;
-
         public HomeWindowViewModel(
             IIdentityStore identityStore,
             IEventAggregator eventAggregator,
             ILocalizationAppService localizationAppService,
-            ISettingsService settingsService)//,
-            //ICompareVersionService compareVersionService)
+            ISettingsService settingsService)
             : base(identityStore, eventAggregator, localizationAppService)
         {
-            //_compareVersionService = compareVersionService;
-
             DownloadStyleUri = new Uri($"{settingsService.GetIdentitytBaseAddress()}style");
             DownloadApplicationUri = new Uri(settingsService.GetApplicationDownloadAddress());
         }
-
-        public VersionDTO CurrentVersion => new VersionDTO { ApplicationVersion = 1.0f, StyleVersion = 1.0f };//_compareVersionService.CurrentVersion;
-        public VersionDTO AvailableVersion => new VersionDTO { ApplicationVersion = 1.0f, StyleVersion = 1.0f };//_compareVersionService.AvailableVersion;
-        public bool NewStyleAvailable => false; //_compareVersionService.NewStyleVersionAvailable();
-        public bool NewApplicationAvailable => false;//_compareVersionService.NewApplicationVersionAvailable();
 
         public Uri DownloadStyleUri { get; }
         public Uri DownloadApplicationUri { get; }
