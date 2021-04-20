66ae107c34047aaa7a4e2591605278f669015559
Matteo Piazze <m.piazze-alten@esaote.com>
2021-04-20 14:29:24 +0200

    Fix loginToolBar

---
+++ Client/Globe.Client.Localizer/Views/LoginWindowToolBar.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Globe.Client.Platform.Controls;
+
+namespace Globe.Client.Localizer.Views
+{
+    /// <summary>
+    /// Interaction logic for LoginWindowToolBar.xaml
+    /// </summary>
+    public partial class LoginWindowToolBar : ContextToolBar
+    {
+        public LoginWindowToolBar()
+        {
+            InitializeComponent();
+        }
+    }
+}
